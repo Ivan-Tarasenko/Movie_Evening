d82bc22d8e6cbef1921621b6eeb75b726d0936bc
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2023-10-07 20:10:08 +0700

    fix

--- Movie Evening/Services/Network/Constants.swift
+++ Movie Evening/Services/Network/Constants.swift
@@ -11,8 +11,8 @@ enum Constants {
     static let baseURL = "https://api.kinopoisk.dev/v1.3/"
     static let path = "movie"
 //    static let apiKey = "73RGG0S-NPBM7JF-G735BVT-01XY53G"
-//    static let apiKey = "P61F8Q7-P6T4AY8-KQYYAQ8-W5DEH32"
-    static let apiKey = "BZGNQX5-N9QM1X6-JZYG5MY-TNA0584"
+    static let apiKey = "P61F8Q7-P6T4AY8-KQYYAQ8-W5DEH32"
+//    static let apiKey = "BZGNQX5-N9QM1X6-JZYG5MY-TNA0584"
     
     static let requestedGenres = ["комедия", "мультфильм", "ужасы", "приключения", "боевик", "фантастика", "триллер", "мелодрама", "детектив", "фэнтези", "военный", "семейный", "аниме", "история", "драма", "документальный", "детский", "криминал", "биография", "вестерн", "фильм-нуар", "спорт", "мюзикл", "ток-шоу", "игра"]
 }
